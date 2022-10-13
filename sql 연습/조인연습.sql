bd8d9b6ec4f5d4e50e999d66fb57a1f2d8439796
Son-Sumin <goldkitty007@gmail.com>
2022-10-13 15:23:18 +0900

    join #inner

--- sql 연습/조인연습.sql
+++ sql 연습/조인연습.sql
@@ -33,34 +33,64 @@ select a.first_name, b.title   -- employees.first_name 너무 기니까
 -- natural join의 문제점(이름이 같은 컬럼이 여러 개일 때) 해결을 위해 사용
 select count(*)
   from salaries a natural join titles b
- where b.to_date = '1999-01-01';
+ where b.to_date = '9999-01-01';
  
 select count(*)
   from salaries a join titles b using(emp_no)
- where b.to_date = '1999-01-01';
+ where b.to_date = '9999-01-01';
  
 -- 3) join ~ on
 -- 예제: 현재 직책별 평균 연봉을 큰 순서대로 출력하세요.
 select b.title, avg(salary)
   from salaries a 
   join titles b on a.emp_no = b.emp_no
- where b.to_date = '1999-01-01'
-   and a.to_date = '1999-01-01'
+ where b.to_date = '9999-01-01'
+   and a.to_date = '9999-01-01'
  group by b.title
  order by avg(salary) desc;
 
 -- 실습문제1: 
 -- 현재 회사생활을 반영한 직원별 근무부서를 사번, 직원 전체이름, 근무부서 순으로 출력하세요.
-select a.emp_no, concat(a.first_name, a.last_name), 
-  from employees a join dept_manager b on a.emp_no = b.emp_no
- where b.to_date = '1999-01-01'
-group by 
+   select a.emp_no, concat(a.first_name, a.last_name), c.dept_name 
+     from employees a join dept_manager b join departments c
+	   on a.emp_no = b.emp_no and b.dept_no = c.dept_no
+    where b.to_date = '9999-01-01'
+ group by b.emp_no;
+
+select a.emp_no, a.first_name, b.dept_name
+  from employees a, departments b, dept_emp c
+ where a.emp_no = c.emp_no
+   and b.dept_no = c.dept_no
+   and c.to_date = '9999-01-01';
 
 -- 실습문제2: 
--- 
+-- 현재 회사에서 지급되고 있는 급여체계를 반영한 결과를 출력하세요.
+-- 사번, 전체이름, 연봉 형태로 출력하세요.
+select a.emp_no, a.first_name, b.salary
+  from employees a, salaries b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01';
 
--- 실습문제3: 
--- 
+-- 실습문제3:
+-- 현재 직책별로 평균 연봉과 인원수를 구하되,
+-- 직책별로 인원이 100명 이상인 직책만 출력 하세요.
+select a.title, avg(b.salary), count(*)
+  from titles a, salaries b
+ where a.emp_no = b.emp_no
+ group by a.title
+ having count(*) >= 100;
 
 -- 실습문제4: 
--- 
+-- 현재 부서별로 직책이 Engineer인 직원들에 대해서만 평균급여를 구하세요.
+-- 부서이름, 평균급여로 출혁하세요.
+  select a.dept_name, avg(salary)
+    from departments a, dept_emp b, salaries c, titles d
+   where a.dept_no = b.dept_no
+     and b.emp_no = c.emp_no
+     and c.emp_no = d.emp_no
+     and b.to_date = '9999-01-01'
+	 and c.to_date = '9999-01-01'
+	 and d.to_date = '9999-01-01'
+	 and d.title = 'Engineer'
+group by a.dept_no
+order by avg(salary) desc;
