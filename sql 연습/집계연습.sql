c52614758c05351abbeadaf3edf3b81ffede3cd8
Son-Sumin <goldkitty007@gmail.com>
2022-10-13 13:19:04 +0900

    집계함수 final

--- sql 연습/집계연습.sql
+++ sql 연습/집계연습.sql
@@ -31,4 +31,19 @@ select emp_no, avg(salary) as avg_salary
   having avg_salary > 60000
   order by avg_salary desc;
   
-  -- 입력순서: from, where, group by, having, orderby
+-- 입력순서: from, where, group by, having, orderby
+
+-- 예제
+-- salaries 테이블에서 사번이 10060인 직원의 사번, 급여평균, 급여총합을 출력하세요.
+
+-- 문법적으로 오류!!
+-- 의미적으로는 맞다
+select emp_no, avg(salary), sum(salary)
+  from salaries
+  where emp_no = '10060';
+
+-- 문법적으로 True
+select emp_no, avg(salary), sum(salary)
+  from salaries
+  group by emp_no
+  having emp_no = '10060';
