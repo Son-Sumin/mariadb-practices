d444b840e6dfaa2997f97a9e4302bfb117f5ea97
Son-Sumin <goldkitty007@gmail.com>
2022-10-14 10:23:12 +0900

    outer join

---
+++ sql 연습/1014 수업.sql
@@ -0,0 +1,12 @@
+-- inner join ; join 조건에 만족하는 얘들만 출력
+-- 종류; equi join, join on(표준), natural join(표준), join ~ using(표준)
+
+-- outer join ; join 조건에 만족하는 얘들과 조건에 맞지 않는 얘들도 출력
+-- 종류 ; left join on(표준), right join on(표준), full join on(표준)
+
+-- 식별(실선) vs 비식별(점선)
+-- 비식별; A의 테이블 PK가 B의 테이블 FK가 될 때
+-- 식별; A의 테이블 PK가 B의 테이블 PK도 될 때
+
+-- forward; erd -> 물리
+-- reverse; 물리 -> erd
