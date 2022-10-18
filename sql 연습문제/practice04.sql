2b3549697d357e41b6dc66392f7d1bd98098cb7e
Son-Sumin <goldkitty007@gmail.com>
2022-10-18 19:23:47 +0900

    answer

--- sql 연습문제/practice04.sql
+++ sql 연습문제/practice04.sql
@@ -7,11 +7,28 @@ select avg(salary) as avg_salary
  where to_date = '9999-01-01';
  
  -- 72012.2359
- select count(*) as emp_avgsal
+ select count(*)
    from salaries
-  where salary > (select avg(salary) as avg_salary
+  where to_date = '9999-01-01'
+    and salary > (select avg(salary) as avg_salary
 					from salaries
                    where to_date = '9999-01-01');
+                   
+-- sol2)
+SELECT 
+    COUNT(*)
+FROM
+    salaries
+WHERE
+    to_date = '9999-01-01'
+        AND salary > (SELECT 
+            AVG(salary)
+        FROM
+            salaries
+        WHERE
+            to_date = '9999-01-01');
+
+-- ------------------------------------------------------------------------------
 
 -- 문제2. 
 -- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 연봉을 조회하세요.
@@ -24,7 +41,7 @@ select b.dept_no, max(a.salary) as max_salary
  group by b.dept_no;
  
 -- sol1) where절 subquery
-select d.emp_no, d.first_name, b.dept_no, a.salary
+select d.emp_no, d.first_name, c.dept_name, a.salary
   from salaries a, dept_emp b, departments c, employees d 
  where a.emp_no = b.emp_no
    and b.dept_no = c.dept_no
@@ -38,10 +55,10 @@ select d.emp_no, d.first_name, b.dept_no, a.salary
                                     and b.to_date = '9999-01-01'
 							   group by b.dept_no)
   group by b.dept_no
-  order by a.salary asc;
+  order by a.salary desc;
 
 -- sol2) from절 subquery
-  select d.emp_no, d.first_name, b.dept_no, a.salary
+  select d.emp_no, d.first_name, c.dept_name, a.salary
   from salaries a, dept_emp b, departments c, employees d,
        (select b.dept_no, max(a.salary) as max_salary
 								   from salaries a, dept_emp b
@@ -56,8 +73,40 @@ select d.emp_no, d.first_name, b.dept_no, a.salary
    and a.to_date = '9999-01-01'
    and b.to_date = '9999-01-01'
   group by b.dept_no
-  order by max_salary asc;
+  order by max_salary desc;
   
+  
+-- sol3)
+SELECT 
+    a.emp_no,
+    CONCAT(a.first_name, ' ', a.last_name),
+    d.dept_name,
+    b.salary
+FROM
+    employees a,
+    salaries b,
+    dept_emp c,
+    departments d,
+    (SELECT 
+        c.dept_no, max(b.salary) AS max_salary
+    FROM
+        employees a, salaries b, dept_emp c
+    WHERE
+        a.emp_no = b.emp_no
+            AND a.emp_no = c.emp_no
+            AND b.to_date = '9999-01-01'
+            AND c.to_date = '9999-01-01'
+    GROUP BY c.dept_no) e
+WHERE
+    a.emp_no = b.emp_no
+        AND a.emp_no = c.emp_no
+        AND c.dept_no = e.dept_no
+        AND b.salary = e.max_salary
+        AND e.dept_no = d.dept_no
+        AND b.to_date = '9999-01-01'
+        AND c.to_date = '9999-01-01'
+ORDER BY b.salary DESC;    
+  -- ------------------------------------------------------------------------------
 
 -- 문제3.
 -- 현재, 자신의 부서 평균 급여보다 연봉(salary)이 많은 사원의 사번, 이름과 연봉을 조회하세요.
@@ -81,7 +130,33 @@ select c.emp_no, c.first_name, a.salary, b.dept_no
                and a.to_date = '9999-01-01'
                and b.to_date = '9999-01-01'
           group by b.dept_no);
-   
+          
+-- sol2)
+SELECT 
+    a.emp_no, CONCAT(a.first_name, ' ', a.last_name), b.salary
+FROM
+    employees a,
+    salaries b,
+    dept_emp c,
+    (SELECT 
+        c.dept_no, AVG(b.salary) AS avg_salary
+    FROM
+        employees a, salaries b, dept_emp c
+    WHERE
+        a.emp_no = b.emp_no
+            AND a.emp_no = c.emp_no
+            AND b.to_date = '9999-01-01'
+            AND c.to_date = '9999-01-01'
+    GROUP BY c.dept_no) d
+WHERE
+    a.emp_no = b.emp_no
+        AND a.emp_no = c.emp_no
+        AND c.dept_no = d.dept_no
+        AND b.salary > d.avg_salary
+        AND b.to_date = '9999-01-01'
+        AND c.to_date = '9999-01-01';
+  
+ -- ------------------------------------------------------------------------------ 
    
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
@@ -101,6 +176,8 @@ select a.emp_no, a.first_name, d.manager_name, c.dept_name
  where a.emp_no = b.emp_no
    and b.dept_no = c.dept_no
    and b.to_date = '9999-01-01';
+
+-- ------------------------------------------------------------------------------
 
 -- 문제5. 000
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
@@ -144,6 +221,40 @@ select a.emp_no, a.first_name, e.dept_nono, c.title, d.salary
    and c.to_date = '9999-01-01'
    and d.to_date = '9999-01-01'
 order by d.salary desc;
+
+-- sol2)
+SELECT 
+    a.emp_no,
+    CONCAT(a.first_name, ' ', a.last_name) AS name,
+    b.title,
+    c.salary
+FROM
+    employees a,
+    titles b,
+    salaries c,
+    dept_emp d
+WHERE
+    a.emp_no = b.emp_no
+        AND a.emp_no = c.emp_no
+        AND a.emp_no = d.emp_no
+        AND b.to_date = '9999-01-01'
+        AND c.to_date = '9999-01-01'
+        AND d.to_date = '9999-01-01'
+        AND d.dept_no = (SELECT 
+            dept_no
+        FROM
+            (SELECT 
+                dept_no, AVG(salary) AS avg_salary
+            FROM
+                salaries a, dept_emp b
+            WHERE
+                a.emp_no = b.emp_no
+                    AND a.to_date = '9999-01-01'
+                    AND b.to_date = '9999-01-01'
+            GROUP BY dept_no
+            ORDER BY avg_salary DESC
+            LIMIT 0 , 1) a)
+ORDER BY c.salary DESC;
 
 -- 문제6.
 -- 평균 연봉이 가장 높은 부서는? 
@@ -233,3 +344,31 @@ select c.dept_name, d.first_name, a.salary, e.manager_name, f.sal_manager
    and b.to_date = '9999-01-01'
 group by f.manager_no
   having a.salary > f.sal_manager;
+  
+-- sol2)
+SELECT 
+    f.dept_name AS '부서이름',
+    a.first_name AS '사원이름',
+    d.salary AS '연봉',
+    g.first_name AS '매니저 이름',
+    e.salary AS '매니저 연봉'
+FROM
+    employees a,
+    dept_emp b,
+    dept_manager c,
+    salaries d,
+    salaries e,
+    departments f,
+    employees g
+WHERE
+    a.emp_no = b.emp_no
+        AND c.dept_no = b.dept_no
+        AND a.emp_no = d.emp_no
+        AND c.emp_no = e.emp_no
+        AND c.dept_no = f.dept_no
+        AND c.emp_no = g.emp_no
+        AND b.to_date = '9999-01-01'
+        AND c.to_date = '9999-01-01'
+        AND d.to_date = '9999-01-01'
+        AND e.to_date = '9999-01-01'
+        AND d.salary > e.salary;
