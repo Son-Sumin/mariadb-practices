e22fb8e7d6b48e5a06ed4c5c681bb488463539d6
Son-Sumin <goldkitty007@gmail.com>
2022-10-18 14:43:25 +0900

    bookmall

---
+++ mysqlclient-practices/bookmall/main.py
@@ -0,0 +1,20 @@
+from models import model_member
+
+
+print("--회원 리스트--")
+model_member.insert()
+model_member.insert()
+results = model_member.findall()
+for result in results:
+    print(result)
+
+
+print("--카테고리 리스트--")
+
+print("--상품 리스트--")
+
+print("--카트 리스트--")
+
+print("--주문 리스트--")
+
+print("--주문도서 리스트--")
---
+++ mysqlclient-practices/bookmall/models/model_member.py
@@ -0,0 +1,6 @@
+def insert():
+    print('member 추가')
+
+
+def findall():
+    print('memberlist 가져오기')
