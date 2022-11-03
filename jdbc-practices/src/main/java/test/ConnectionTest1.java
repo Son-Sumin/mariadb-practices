82dd3b38093d169e3d6e027777cc7cc6e363624f
Son-Sumin <goldkitty007@gmail.com>
2022-11-04 08:40:39 +0900

    JDBC 시작

--- jdbc-practices/src/main/java/test/ConnectionTest1.java
+++ jdbc-practices/src/main/java/test/ConnectionTest1.java
@@ -1,5 +1,11 @@
 /*
  * Maven Project로 jdbc-practices 만들기
+ * JDBC; 자바 프로그램 내에서 SQL문을 실행하기 위한 자바API(Java Library Interface)
+ * insert, delete, update, select는 여러 드라이버 설치 시 이용
+ * executeUpdate -> insert, delete, update
+ * executeQuery -> select
+ *    -> ResultSet rs = stmt.executeQuery(SQL); while(rs.next()); sysout ~
+ * 
  * pom; <properties></properties> <dependencies><dependency></dependency></dependencies> <build></build> 수정
  * <dependency></dependency>은 MariaDB Java Client googling -> 원하는 버전 copy&paste
  * ignore 이클립스, 파이참 등 사용 언어 관련 기본 파일 확장자 적기
