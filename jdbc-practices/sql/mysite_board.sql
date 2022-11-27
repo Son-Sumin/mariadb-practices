fab6399143ea7ff371a2a56ab86681a3c0a174aa
Son-Sumin <goldkitty007@gmail.com>
2022-11-27 15:49:19 +0900

    mysite03

--- jdbc-practices/sql/mysite_board.sql
+++ jdbc-practices/sql/mysite_board.sql
@@ -44,24 +44,32 @@ select a.no, a.title, a.hit,
  where a.user_no = b.no
 order by group_no desc, order_no asc;
 
+select * from board;
+
 -- insert
 insert into board values(null, '베스킨', '사빠딸', '1', now(), '1', '1', '0', '6');
 insert into board values(null, '빠바', '소시지빵', '1', now(), 'maxGroupNo+1', '1', '0', '6');
 insert into board values(null, '방앗간', '인절미', '1', now(), 'max(group_no) as maxGroupNo+1', '1', '0', '7');
 insert into board values(null, '도미노피자', '슈프림피자', '1', now(), '1', '1', '1', '4');
 insert into board values(null, '아', '샷츄', '0', now(), (select ifnull(max(group_no), 1))+1, '1', '0', '6');
+select ifnull(max(group_no), 1) from board;
+
+select * from board;
 
 -- primary key를 null이 아닌 직접 insert로 사용하고 싶을 때
 select last_insert_id();
 
 -- delete
 delete from board where title = '빠바';
-select ifnull(max(group_no), 1) from board;
+
+select * from board;
 
 -- view
 select no, title, contents, hit, user_no
   from board
  where no = 3;
+ 
+select * from board;
  
 -- update
 update board
@@ -69,7 +77,25 @@ update board
        contents='붕어빵 수정'
  where no=5;
 
+select * from board;
+
 -- updateHit
 update board
    set hit = hit +1
  where no = 3;
+
+select * from board;
+ 
+ -- updateOrderNo
+update board
+   set group_no = group_no,
+       order_no = order_no + 1
+ where no = 8;
+
+-- 확인 중
+update board
+   set order_no = order_no + 1
+ where group_no = 1
+   and order_no >= 1;
+
+select * from board;
